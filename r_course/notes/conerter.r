817fa7cad44ba4dd7b1415d1147862c8962b557b
Thomas Carroll <tc.infomatics@gmail.com>
2015-02-09 19:54:11 +0000

    .

--- r_course/notes/conerter.r
+++ r_course/notes/conerter.r
@@ -1,7 +1,7 @@
-testC <- readChar("/Users/tcarroll/Downloads/htqc/r_course/convertion.md", file.info("/Users/tcarroll/Downloads/htqc/r_course/convertion.md")$size)
+testC <- readChar("/Users/tcarroll/Downloads/htqc/r_course/introToR_Session1.md", file.info("/Users/tcarroll/Downloads/htqc/r_course/introToR_Session1.md")$size)
 testCC <- strsplit(testC,"\n")
 presBreaks <- grep("^=*=$",testCC[[1]])
-testCC[[1]][grep("^=*=$",testCC[[1]])-1] <- paste0("##",
+testCC[[1]][grep("^=*=$",testCC[[1]])-1] <- paste0("###",
   testCC[[1]][grep("^=*=$",testCC[[1]])-1])
 testCC[[1]][grep("^=*=$",testCC[[1]])] <- "\n"
 lapply(testCC[[1]], cat, "\n", file="test.md", append=TRUE)
