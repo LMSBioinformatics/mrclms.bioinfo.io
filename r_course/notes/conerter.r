e9848cb3f42aa4f1f6c4c40dbe0736bf4b70ef6d
Thomas Carroll <tc.infomatics@gmail.com>
2015-02-17 18:53:42 +0000

    .

--- r_course/notes/conerter.r
+++ r_course/notes/conerter.r
@@ -1,5 +1,5 @@
 library(IRanges)
-testC <- readChar("/Users/tcarroll/Downloads/htqc/r_course/introToR_Session2.md", file.info("/Users/tcarroll/Downloads/htqc/r_course/introToR_Session2.md")$size)
+testC <- readChar("/Users/tcarroll/Downloads/htqc/r_course/Bioconductor//Bioconductor_Tutorial.md", file.info("/Users/tcarroll/Downloads/htqc/r_course/Bioconductor//Bioconductor_Tutorial.md")$size)
 testCC <- strsplit(testC,"\n")
 presBreaks <- grep("^=*=$",testCC[[1]])
 presStarts <- presBreaks
@@ -29,7 +29,7 @@ testCC[[1]][grep("^=.*.=$",testCC[[1]])] <- "\n"
 
 
 testCC[[1]][lefts] <- "\n"
-unlink("/Users/tcarroll/Downloads/htqc/r_course/test.md")
-lapply(testCC[[1]], cat, "\n", file="/Users/tcarroll/Downloads/htqc/r_course/test.md", append=TRUE)
+unlink("/Users/tcarroll/Downloads/htqc/r_course/Bioconductor/Bioconductor_Tutorial_Document.md")
+lapply(testCC[[1]], cat, "\n", file="/Users/tcarroll/Downloads/htqc/r_course/Bioconductor/Bioconductor_Tutorial_Document.md", append=TRUE)
 getwd()
 
